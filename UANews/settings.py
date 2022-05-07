8903a4740ad6818a4e1b1e27993a7f3a37137b7e
Antoniy <viterantoniy20@gmail.com>
2022-05-07 22:27:08 +0200

    fgdngfdsddfghdfgh

--- UANews/settings.py
+++ UANews/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a+nc&45)0l9q%f)hz%@05nlr+psmrx@91lb&ijm*9a70nv^c%6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
